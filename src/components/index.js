72eaf8acdb216853076dfa1dd22f645c834136c7
gustavomatheus <gustavo.matheus@revmob.com>
2020-04-10 21:45:30 -0300

    components/index created

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header'
+export { default as Content } from './Content'
+export { default as Form } from './Form'
+export { default as Answers } from './Answers'
